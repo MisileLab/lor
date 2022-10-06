c623f748bab50562ef5f4b4317d7b3613719963b
MisileLaboratory <misileminecord@gmail.com>
2022-10-07 01:13:38 +0900

    Update somelight.py

--- prescripts/4/somelight.py
+++ prescripts/4/somelight.py
@@ -1,10 +1,8 @@
-# Imports go at the top
 from microbit import *
 from library import *
 
 a = 0
-    
-# Code in a 'while True:' loop repeats forever
+
 while True:
     if button_one_time_pressed(button_a) or button_one_time_pressed(button_b):
         a += 1
@@ -20,35 +18,35 @@ while True:
                         . . # . .
         """))
     elif a < 30:
-        display.show("""
+        display.show(from_block_to_python("""
             . # . # .
                         # . # . #
                         # # . # #
                         . # # # .
                         . . # . .
-        """)
+        """))
     elif a < 40:
-        display.show("""
+        display.show(from_block_to_python("""
             . # . # .
                         # . . . #
                         # . . . #
                         . # # # .
                         . . # . .
-        """)
+        """))
     elif a < 50:
-        display.show("""
+        display.show(from_block_to_python("""
             . . . . .
                         . . . . .
                         . . . . .
                         . . . . .
                         . . # . .
-        """)
+        """))
     else:
-        display.show("""
+        display.show(from_block_to_python("""
             . . . . .
                         . . . . .
                         . . . . .
                         . . . . .
                         . . . . .
-        """)
+        """))
 
