05169d0371886a745888d52cc3e73334eef55871
MisileLaboratory <misileminecord@gmail.com>
2022-10-22 01:51:08 +0900

    Update library.py

--- prescripts/4/library.py
+++ prescripts/4/library.py
@@ -4,6 +4,17 @@ def button_one_time_pressed(button: Button):
     return button.is_pressed() and (button.was_pressed() != False)
 
 def from_block_to_python(s: str, level: int = 9):
+    a = 1
+    b = 0
+    st = ""
+    for i in s:
+        if a % 5 == 0 and b != 4:
+            st = st + i + ":"
+            a = 0
+            b += 1
+        else:
+            st = st + i
+        a += 1
     return Image(s.replace('.', '0').replace('#', str(level)))
 
 class Sprite:
