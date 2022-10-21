4fe91a71ce60ec3ce0d0f8425b6a0f8b4e99c928
MisileLaboratory <misileminecord@gmail.com>
2022-10-22 01:43:04 +0900

    Update 1.py

--- prescripts/4/1.py
+++ prescripts/4/1.py
@@ -5,5 +5,5 @@ sprite = Sprite(2, 2)
 while True:
     if button_one_time_pressed(button_a):
         sprite.add(1)
-    elif button_one_time_pressed(button_b):
+    if button_one_time_pressed(button_b):
         sprite.substract(1)
