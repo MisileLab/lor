aa1a50611a9113f22a1a62f01b7a2527fcdd8d7f
misilelaboratory <misileminecord@gmail.com>
2022-11-21 19:05:42 +0900

    some add

--- prescripts/6/main.py
+++ prescripts/6/main.py
@@ -2,8 +2,8 @@ from matplotlib import pyplot
 from misilelibpy import read_once
 from json import loads
 
-a = loads(read_once("test.json"))
+a = loads(read_once("data.json"))
 
-pyplot.plot(a["data"])
+pyplot.plot(range(len(a["data"])), a["data"], a["timedata"])
 pyplot.show()
 
--- prescripts/6/validate.py
+++ prescripts/6/validate.py
@@ -2,16 +2,33 @@ from json import dumps, loads
 from misilelibpy import read_once, write_once
 from sys import stdin
 from subprocess import run
+from time import time
 
-a = loads(read_once('test.json'))
+a = loads(read_once('data.json'))
 b = 0
+c = True
+timedata = time()
 
-for i in stdin:
-    if i.replace('\n', '') == "show":
-        b += 1
-        run("vim test.txt")
+while c:
+    try:
+        for i in stdin:
+            if i.replace('\n', '') == "show":
+                b += 1
+                run("vim test.txt", shell=True)
+            elif i == "":
+                c = False
+                break
+    except KeyboardInterrupt:
+        an = input("yes? y is yes")
+        if an == "y":
+            c = False
+            break
+    except Exception:
+        pass
+    
 
 print(b)
 a["data"].append(b)
-write_once('test.json', dumps(a))
+a["timedata"].append(time() - timedata)
+write_once('data.json', dumps(a))
 
